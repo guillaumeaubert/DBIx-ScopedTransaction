a26bfc3db2c2217ed191879e82d6e0d6e374c8fe
Guillaume Aubert <aubertg@cpan.org>
2013-02-24 11:44:27 -0500

    Released version 1.1.1.

--- lib/DBIx/ScopedTransaction.pm
+++ lib/DBIx/ScopedTransaction.pm
@@ -15,11 +15,11 @@ DBIx::ScopedTransaction - Scope database transactions on DBI handles in code, to
 
 =head1 VERSION
 
-Version 1.1.0
+Version 1.1.1
 
 =cut
 
-our $VERSION = '1.1.0';
+our $VERSION = '1.1.1';
 
 our $DESTROY_LOGGER;
 
--- t/lib/LocalTest.pm
+++ t/lib/LocalTest.pm
@@ -14,11 +14,11 @@ LocalTest - Test functions for L<DBIx::ScopedTransaction>.
 
 =head1 VERSION
 
-Version 1.1.0
+Version 1.1.1
 
 =cut
 
-our $VERSION = '1.1.0';
+our $VERSION = '1.1.1';
 
 
 =head1 SYNOPSIS
